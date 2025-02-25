af1aa97542d9b7a335c603d64912a958cf6298e7
use <use@DESKTOP-3QVJ858.mshome.net>
2025-02-25 16:51:36 +0530

    Added searchtextfield in product page

--- ComcastVtigerFramework/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
+++ ComcastVtigerFramework/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
@@ -7,9 +7,18 @@ public class ProductsPage
 {
 	@FindBy(xpath="//input[@alt='Create Product...']")
 	private WebElement createProductImgBtn;
+	
+	@FindBy(name="search_text")
+	private WebElement searchtxtedt;
 
 	public WebElement getCreateProductImgBtn() {
 		return createProductImgBtn;
+		
+		
+	}
+
+	public WebElement getSearchtxtedt() {
+		return searchtxtedt;
 	}
 	
 }
